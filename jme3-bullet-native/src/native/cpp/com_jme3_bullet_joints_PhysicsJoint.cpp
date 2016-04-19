f532cc9a23eee60d320d676fbe8faa561589ccb9
Dokthar <jmaselbas@gmail.com>
2016-04-20 00:12:47 +0200

    bullet : added missing finalize native method

--- jme3-bullet-native/src/native/cpp/com_jme3_bullet_joints_PhysicsJoint.cpp
+++ jme3-bullet-native/src/native/cpp/com_jme3_bullet_joints_PhysicsJoint.cpp
@@ -55,7 +55,23 @@ extern "C" {
         }
         return joint->getAppliedImpulse();
     }
-
+    
+    /*
+     * Class:     com_jme3_bullet_joints_PhysicsJoint
+     * Method:    finalizeNative
+     * Signature: (J)V
+     */
+    JNIEXPORT void JNICALL Java_com_jme3_bullet_joints_PhysicsJoint_finalizeNative
+    (JNIEnv *env, jobject object, jlong jointId) {
+        btTypedConstraint* joint = reinterpret_cast<btTypedConstraint*>(jointId);
+        if (joint == NULL) {
+            jclass newExc = env->FindClass("java/lang/NullPointerException");
+            env->ThrowNew(newExc, "The native object does not exist.");
+            return;
+        }
+        delete(joint);
+    }
+    
 #ifdef __cplusplus
 }
 #endif
